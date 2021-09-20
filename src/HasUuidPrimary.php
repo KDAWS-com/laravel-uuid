b2a747cdd5706e1aae29573edd1b4fb250e9b37a
Karl Austin <karl.austin@kdaws.com>
2021-09-20 10:15:00 +0100

    Initial commit

---
+++ src/HasUuidPrimary.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace KDAWScom\LaravelUuid;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+/**
+ * Trait HasUuidPrimary
+ *
+ * The model has an ordered UUID primary key
+ *
+ * @package KDAWScom\LaravelUuid
+ */
+trait HasUuidPrimary
+{
+
+    /**
+     * @return string
+     */
+    public function getKeyType()
+    {
+        return 'string';
+    }
+
+    /**
+     * @return bool
+     */
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    /**
+     * @return void
+     */
+    public static function bootHasUuidPrimary()
+    {
+        /**
+         * Make sure the model is using string PK and not auto incrementing
+         *
+         * If there is no key currently, generate a new UUID key
+         */
+        static::creating(function (Model $model) {
+            $model->keyType = 'string';
+            $model->incrementing = false;
+
+            if (!$model->getKey()) {
+                $model->{$model->getKeyName()} = (string)Str::orderedUuid();
+            }
+        });
+
+        /**
+         * We check using an attribute for the key rather than the getKey() as getKey() won't hold the new value yet
+         */
+        static::saving(function (Model $model) {
+            $originalKey = $model->getOriginal($model->getKeyName());
+            if (!is_null($originalKey)) {
+                if ($originalKey !== $model->{$model->getKeyName()}) {
+                    $model->{$model->getKeyName()} = $originalKey;
+                }
+            }
+        });
+    }
+}
---
+++ src/HasUuidSecondary.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace KDAWScom\LaravelUuid;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+/**
+ * Trait HasUuidPrimary
+ *
+ * The model has an ordered UUID secondary key
+ *
+ * @package KDAWScom\LaravelUuid
+ */
+trait HasUuidSecondary
+{
+
+    /**
+     * Are we protecting the UUID from change?
+     *
+     * @var bool
+     */
+    protected $laravelUuidSecondaryLocked = true;
+
+    protected $laravelUuidSecondaryKeyName = 'uuid';
+
+    /**
+     * @return void
+     */
+    public static function bootHasUuidSecondary()
+    {
+        /**
+         * If there is no key currently, generate a new UUID key
+         */
+        static::creating(function (Model $model) {
+            if (!$model->{$this->laravelUuidSecondaryKeyName}) {
+                $model->{$this->laravelUuidSecondaryKeyName} = (string)Str::orderedUuid();
+            }
+        });
+
+        /**
+         * Check to see if we are protecting the UUID key against change.  If we are, reset the key value back to
+         * the original value.
+         */
+        static::saving(function (Model $model) {
+            $originalKey = $model->getOriginal($model->{$this->laravelUuidSecondaryKeyName});
+            if (!is_null($originalKey) && $model->laravelUuidSecondaryLocked) {
+                if ($originalKey !== $model->{$this->laravelUuidSecondaryKeyName}) {
+                    $model->{$this->laravelUuidSecondaryKeyName} = $originalKey;
+                }
+            }
+        });
+    }
+}
